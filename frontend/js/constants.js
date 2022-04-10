5ccc15089fe654e22b5d164cc6bf08d134bd659c
lomeli-noe <neolomeli@gmail.com>
2022-04-09 20:44:44 -0700

    Deploy Contract

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x1DE5A9Fb45ad4803BB12278c92a7ea5D1E2e283D"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
